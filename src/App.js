d2a198fd47dd128dd1d4ab2c44913eda1a86fd1c
maikon_ola_mundo@outlook.com <maikon.muniz@yahoo.com.br>
2021-06-25 17:30:38 -0300

    aula 12 e 13

--- src/App.js
+++ src/App.js
@@ -8,26 +8,65 @@ import Numero from './componentes/numero'
 import Evento from './componentes/superman';
 import './App.css'
 
-
 export default function App() {
 
+  const herois = [
+    
+      {Heroi:'Superman', Nome:'Clark'},
+      {Heroi:'Batman', Nome:'Bruce'}
+      ]
+
+  const listaHerois = herois.map(
+
+    (h, i)=>
+
+      <p key={i}>chave {i} - heroi {h.Heroi}, Nome {h.Nome}</p>
+
+  )
 
   const [num, setNum] = useState(10)
-  const [ligado, setLigado] = useState(true)
+  const [ligado, setLigado] = useState(false)
+  const [log, setLog] = useState(true)
+
+  const comprimento = () => {
+
+    const hora= new Date().getHours()
+
+    if(hora >= 0 && hora < 13){
+
+      return <p>Bom dia!</p>
+
+    }else if(hora >= 13 && hora < 18){
+
+      return <p>Boa Tarde!</p>
+
+
+    }else{
+
+      
+      return <p>Boa noite!</p>
+
+
+    }
+  }
 
   return (
+
     <>
     <section className='caixa'>
 
-
       <Topo />
       <Relogio />
-      <p>O numero é: {num} </p>
+      <p>O numero é: {num}</p>
       <Numero num={num} setNum={setNum}/>
+      {comprimento()}
+      {listaHerois}
+      <p>{log?'Usuario não logado':'Usuario Logado'}</p>
+      <button onClick={()=>setLog(!log)}>{log?'logoff':'login'}</button><br/>
       <Evento ligado={ligado} setLigado={setLigado}/>
       <Corpo />
-
     </section>
     </>
+
 );
 }
